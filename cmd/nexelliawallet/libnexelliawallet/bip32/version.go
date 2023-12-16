9c702055b068b3321c1b63b1564b987e97f765b1
romxxxx <romuald.brune@gmail.com>
2023-12-17 00:36:17 +0100

    Corrections mineures

--- cmd/nexelliawallet/libnexelliawallet/bip32/version.go
+++ cmd/nexelliawallet/libnexelliawallet/bip32/version.go
@@ -22,80 +22,80 @@ var BitcoinMainnetPublic = [4]byte{
 	0x1e,
 }
 
-// nexepaMainnetPrivate is the version that is used for
-// nexepa mainnet bip32 private extended keys.
+// NexelliaMainnetPrivate is the version that is used for
+// Nexellia mainnet bip32 private extended keys.
 // Ecnodes to xprv in base58.
-var nexepaMainnetPrivate = [4]byte{
+var NexelliaMainnetPrivate = [4]byte{
 	0x03,
 	0x8f,
 	0x2e,
 	0xf4,
 }
 
-// nexepaMainnetPublic is the version that is used for
-// nexepa mainnet bip32 public extended keys.
+// NexelliaMainnetPublic is the version that is used for
+// Nexellia mainnet bip32 public extended keys.
 // Ecnodes to kpub in base58.
-var nexepaMainnetPublic = [4]byte{
+var NexelliaMainnetPublic = [4]byte{
 	0x03,
 	0x8f,
 	0x33,
 	0x2e,
 }
 
-// nexepaTestnetPrivate is the version that is used for
-// nexepa testnet bip32 public extended keys.
+// NexelliaTestnetPrivate is the version that is used for
+// Nexellia testnet bip32 public extended keys.
 // Ecnodes to ktrv in base58.
-var nexepaTestnetPrivate = [4]byte{
+var NexelliaTestnetPrivate = [4]byte{
 	0x03,
 	0x90,
 	0x9e,
 	0x07,
 }
 
-// nexepaTestnetPublic is the version that is used for
-// nexepa testnet bip32 public extended keys.
+// NexelliaTestnetPublic is the version that is used for
+// Nexellia testnet bip32 public extended keys.
 // Ecnodes to ktub in base58.
-var nexepaTestnetPublic = [4]byte{
+var NexelliaTestnetPublic = [4]byte{
 	0x03,
 	0x90,
 	0xa2,
 	0x41,
 }
 
-// nexepaDevnetPrivate is the version that is used for
-// nexepa devnet bip32 public extended keys.
+// NexelliaDevnetPrivate is the version that is used for
+// Nexellia devnet bip32 public extended keys.
 // Ecnodes to kdrv in base58.
-var nexepaDevnetPrivate = [4]byte{
+var NexelliaDevnetPrivate = [4]byte{
 	0x03,
 	0x8b,
 	0x3d,
 	0x80,
 }
 
-// nexepaDevnetPublic is the version that is used for
-// nexepa devnet bip32 public extended keys.
+// NexelliaDevnetPublic is the version that is used for
+// Nexellia devnet bip32 public extended keys.
 // Ecnodes to xdub in base58.
-var nexepaDevnetPublic = [4]byte{
+var NexelliaDevnetPublic = [4]byte{
 	0x03,
 	0x8b,
 	0x41,
 	0xba,
 }
 
-// nexepaSimnetPrivate is the version that is used for
-// nexepa simnet bip32 public extended keys.
+// NexelliaSimnetPrivate is the version that is used for
+// Nexellia simnet bip32 public extended keys.
 // Ecnodes to ksrv in base58.
-var nexepaSimnetPrivate = [4]byte{
+var NexelliaSimnetPrivate = [4]byte{
 	0x03,
 	0x90,
 	0x42,
 	0x42,
 }
 
-// nexepaSimnetPublic is the version that is used for
-// nexepa simnet bip32 public extended keys.
+// NexelliaSimnetPublic is the version that is used for
+// Nexellia simnet bip32 public extended keys.
 // Ecnodes to xsub in base58.
-var nexepaSimnetPublic = [4]byte{
+var NexelliaSimnetPublic = [4]byte{
 	0x03,
 	0x90,
 	0x46,
@@ -106,14 +106,14 @@ func toPublicVersion(version [4]byte) ([4]byte, error) {
 	switch version {
 	case BitcoinMainnetPrivate:
 		return BitcoinMainnetPublic, nil
-	case nexepaMainnetPrivate:
-		return nexepaMainnetPublic, nil
-	case nexepaTestnetPrivate:
-		return nexepaTestnetPublic, nil
-	case nexepaDevnetPrivate:
-		return nexepaDevnetPublic, nil
-	case nexepaSimnetPrivate:
-		return nexepaSimnetPublic, nil
+	case NexelliaMainnetPrivate:
+		return NexelliaMainnetPublic, nil
+	case NexelliaTestnetPrivate:
+		return NexelliaTestnetPublic, nil
+	case NexelliaDevnetPrivate:
+		return NexelliaDevnetPublic, nil
+	case NexelliaSimnetPrivate:
+		return NexelliaSimnetPublic, nil
 	}
 
 	return [4]byte{}, errors.Errorf("unknown version %x", version)
@@ -123,13 +123,13 @@ func isPrivateVersion(version [4]byte) bool {
 	switch version {
 	case BitcoinMainnetPrivate:
 		return true
-	case nexepaMainnetPrivate:
+	case NexelliaMainnetPrivate:
 		return true
-	case nexepaTestnetPrivate:
+	case NexelliaTestnetPrivate:
 		return true
-	case nexepaDevnetPrivate:
+	case NexelliaDevnetPrivate:
 		return true
-	case nexepaSimnetPrivate:
+	case NexelliaSimnetPrivate:
 		return true
 	}
 
